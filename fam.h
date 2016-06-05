8cac54e0132599708c692ede83282f981160aa73
imBulbi <mlggrizz3@gmail.com>
2016-06-04 20:15:48 -0400

    Headers work with cpp file

--- fam.h
+++ fam.h
@@ -1,52 +1,59 @@
-#ifndef _INDI_H
-#define _INDI_H
-#endif
+#ifndef FAM_H
+#define FAM_H
+
+#include "indi.h"
+#include <vector>
+
+class Indi;
 
 class Fam {
-    private:
-        int * marr;
-        Indi * husb;
-        Indi * wife;
-        vector <Indi> chil;
-        int * div;
-    public:
-        // Setters:
-        void set_marr (int d, int m, int y) {
-            int arr[] = {d,m,y};
-            this->marr = arr;
-        };
-        void set_husb (Indi h) {
-            this->husb = h;
-        };
-        void set_wife (Indi w) {
-            this->wife = w;
-        };
-        void set_chil (vector<Indi> c) {
-            this->chil = c;
-        };
-        void set_div (int d, int m, int y) {
-            int arr[] = {d,m,y};
-            this->div = arr;
-        };
-        // Getters:
-        int * get_marr () {
-            return this->marr;
-        };
-        Indi * get_husb () {
-            return this->husb;
-        };
-        Indi * get_wife () {
-            return this->wife;
-        };
-        vector <Indi> get_chil () {
-            return this->chil;
-        };
-        int * get_div () {
-            return this->div;
-        };
-        
-        // Other functions
-        void add_chil (Indi c) {
-            chil.push_back(c);
-        };
+private:
+    int * marr;
+    Indi * husb;
+    Indi * wife;
+    std::vector< Indi* > chil;
+    int * div;
+public:
+    
+    
+    // Setters:
+    void set_marr (int d, int m, int y) {
+        int arr[] = {d,m,y};
+        this->marr = arr;
+    }
+    void set_husb (Indi* h) {
+        this->husb = h;
+    }
+    void set_wife (Indi* w) {
+        this->wife = w;
+    }
+    // void set_chil (std::vector<Indi> c) {
+    //     this->chil = c;
+    // }
+    void set_div (int d, int m, int y) {
+        int arr[] = {d,m,y};
+        this->div = arr;
+    }
+    // Getters:
+    int * get_marr () {
+        return this->marr;
+    }
+    inline Indi * get_husb () {
+        return this->husb;
+    }
+    inline Indi * get_wife () {
+        return this->wife;
+    }
+    std::vector< Indi* > get_chil () {
+        return this->chil;
+    }
+    int * get_div () {
+        return this->div;
+    }
+    // Other functions
+    void add_chil (Indi* c) {
+        chil.push_back(c);
+    }
 };
+
+#endif /* Fam */
--- indi.h
+++ indi.h
@@ -1,55 +1,71 @@
-#ifndef _FAM_H
-#define _FAM_H
-#endif
+#ifndef INDI_H
+#define INDI_H
+
+#include "fam.h"
+#include <string>
+#include <vector>
+
+class Fam;
 
 class Indi {
     private:
-        string name;
+        int uniqID;
+        std::string name;
         bool sex;
-        int * birth;
-        int * death;
+        int* birth;
+        int* death;
         Fam * famc;
         Fam * fams;
     public:
+        Indi() {
+            uniqID = -1;
+            name = "";
+            sex = true;
+            birth = new int[3];
+            death = new int[3];
+        }
+
         // Setters:
-        void set_name (string n) {
+        inline void set_name (std::string n) {
             this->name = n;
-        };
-        void set_sex (bool s) {
+        }
+        inline void set_sex (bool s) {
             this->sex = s;
-        };
+        }
         void set_birth (int d, int m, int y) {
             int arr[] = {d,m,y};
             this->birth = arr;
-        };
+        }
         void set_death (int d, int m, int y) {
             int arr[] = {d,m,y};
             this->death = arr;
-        };
-        void set_famc (Fam f){
+        }
+        inline void set_famc (Fam* f){
             this->famc = f;
-        };
-        void set_fams (Fam f) {
+        }
+        inline void set_fams (Fam* f) {
             this->fams = f;
-        };
+        }
         
         // Getters:
-        string get_name () {
+        inline std::string get_name () {
             return this->name;
-        };
-        bool get_sex () {
+        }
+        inline bool get_sex () {
             return this->sex;
-        };
-        int * get_birth () {
+        }
+        inline int * get_birth () {
             return this->birth;
-        };
-        int * get_death () {
+        }
+        inline int * get_death () {
             return this->death;
-        };
-        Fam * get_famc () {
+        }
+        inline Fam * get_famc () {
             return this->famc;
-        };
-        Fam * get_fams () {
+        }
+        inline Fam * get_fams () {
             return this->fams;
-        };
+        }
 };
+
+#endif /* Indi */
