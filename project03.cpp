36a205bc56dbda99af9d4fb767c28e4c5b334e42
imBulbi <mlggrizz3@gmail.com>
2016-06-12 17:14:25 -0400

    US24-Add branch coverage

--- project03.cpp
+++ project03.cpp
@@ -511,7 +511,16 @@ int main() {
                         if (FamArr[restFamID] != NULL) {
                             int currIndiID = FamArr[currID]->get_husb();
                             int nextIndiID = FamArr[restFamID]->get_husb();
-                            if (currIndiID > 0 && nextIndiID > 0) {
+                            if (currIndiID == -1 && nextIndiID == -1) {
+                                currIndiID = FamArr[currID]->get_wife();
+                                nextIndiID = FamArr[restFamID]->get_wife();
+                                if (currIndiID == -1 && nextIndiID == -1) {
+                                    if (dateCompare(FamArr[currID]->get_marr(), FamArr[restFamID]->get_marr()) == 0) {
+                                        cout << "Families " << FamArr[currID]->get_id() << " and " << FamArr[restFamID]->get_id() << " have same marriage dates\n";
+                                    }
+                                }
+                            }
+                            else if (currIndiID > 0 && nextIndiID > 0) {
                                 Indi* currIndi = IndiArr[currIndiID];
                                 Indi* nextIndi = IndiArr[nextIndiID];
                                 if (currIndi != NULL && nextIndi != NULL) {
